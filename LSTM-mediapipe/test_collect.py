a428e8ac09ced8cddcb8ece857913e7e254b35d6
nailuj004 <cruzjulian004@gmail.com>
2024-02-18 15:59:10 +0800

    home.ui

--- LSTM-mediapipe/test_collect.py
+++ LSTM-mediapipe/test_collect.py
@@ -16,7 +16,6 @@ def mediapipe_detection(image, model):
     image.flags.writeable = True                   # Image is now writeable 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
     return image, results
-
 def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION) # Draw face connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
@@ -53,11 +52,11 @@ def extract_keypoints(results):
     return np.concatenate([pose, face, lh, rh])
 
 # Path for exported data, numpy arrays
-path = "C:/Users/hyanx/Documents/Thesis/LSTM-mediapipe/"
+path = "E:/Kaway-Elearn/LSTM-mediapipe/DATASET JULIAN"
 DATA_PATH = os.path.join(path,'MP_Data') 
 print(DATA_PATH)
 # Actions that we try to detect
-actions = np.array(['ano_pangalan_mo', 'ako_si', 'kumusta_ka'])
+actions = np.array(['a', 'b', 'c'])
 
 # Thirty videos worth of data
 no_sequences = 33
@@ -72,7 +71,7 @@ for action in actions:
         except:
             pass
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 # Set mediapipe model 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     
@@ -125,50 +124,50 @@ cap.release()
 cv2.destroyAllWindows()
 
 
-#TRAINING STARTS HERE
-#TRAINING STARTS HERE
-#TRAINING STARTS HERE
-#TRAINING STARTS HERE
-#TRAINING STARTS HERE
-#TRAINING STARTS HERE
-from sklearn.model_selection import train_test_split
-from keras.utils import to_categorical
-
-label_map = {label:num for num, label in enumerate(actions)}
-
-sequences, labels = [], []
-for action in actions:
-    for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int):
-        window = []
-        for frame_num in range(sequence_length):
-            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
-            window.append(res)
-        sequences.append(window)
-        labels.append(label_map[action])
-
-np.array(sequences).shape
-np.array(labels).shape
-X = np.array(sequences)
-X.shape
-y = to_categorical(labels).astype(int)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
-y_test.shape
-
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
-from keras.callbacks import TensorBoard
-
-log_dir = os.path.join('Logs')
-tb_callback = TensorBoard(log_dir=log_dir)
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(actions.shape[0], activation='softmax'))
-
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
-model.summary()
+# #TRAINING STARTS HERE
+# #TRAINING STARTS HERE
+# #TRAINING STARTS HERE
+# #TRAINING STARTS HERE
+# #TRAINING STARTS HERE
+# #TRAINING STARTS HERE
+# from sklearn.model_selection import train_test_split
+# from keras.utils import to_categorical
+#
+# label_map = {label:num for num, label in enumerate(actions)}
+#
+# sequences, labels = [], []
+# for action in actions:
+#     for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int):
+#         window = []
+#         for frame_num in range(sequence_length):
+#             res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+#             window.append(res)
+#         sequences.append(window)
+#         labels.append(label_map[action])
+#
+# np.array(sequences).shape
+# np.array(labels).shape
+# X = np.array(sequences)
+# X.shape
+# y = to_categorical(labels).astype(int)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+# y_test.shape
+#
+# from keras.models import Sequential
+# from keras.layers import LSTM, Dense
+# from keras.callbacks import TensorBoard
+#
+# log_dir = os.path.join('Logs')
+# tb_callback = TensorBoard(log_dir=log_dir)
+# model = Sequential()
+# model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+# model.add(LSTM(128, return_sequences=True, activation='relu'))
+# model.add(LSTM(64, return_sequences=False, activation='relu'))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(actions.shape[0], activation='softmax'))
+#
+# model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+# model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
+# model.summary()
 
