3a29af32bf4e3571675729673feecd09b3881a52
JoshuaPetersonGonzales <157002760+JoshuaPetersonGonzales@users.noreply.github.com>
2024-05-10 21:34:46 +0800

    Mod3 done
    added remaining words to mod 3 ready for training

--- LSTM-mediapipe/test_collect.py
+++ LSTM-mediapipe/test_collect.py
@@ -56,7 +56,7 @@ path = os.getcwd()
 DATA_PATH = os.path.join(path,'MP_Mod3') 
 print(DATA_PATH)
 # Actions that we try to detect
-actions = np.array(['Kumusta ka'])
+actions = np.array(['Magandang araw'])
 
 # Thirty videos worth of data
 no_sequences = 33
